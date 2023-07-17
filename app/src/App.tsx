ca4c8c334aafd3388af3008e25fd8b3ffe2a1163
borzenx <wojtek.117@interia.eu>
2023-07-17 12:05:10 +0200

    Update App.tsx

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React XDDDDDRTHDDX
+          Learn React XDwefewqfq3wefDDDDRTHDDX
         </a>
       </header>
     </div>
