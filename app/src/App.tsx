c281777819bb9f8f0890d4036753ce90c87538ae
borzenx <wojtek.117@interia.eu>
2023-08-04 18:01:07 +0200

    added preprod login

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { Navigation } from './components/navigation';
 import { Home } from './pages/home';
 import { Second } from './pages/second';
+import Login from './components/login';
 import './Sass/main.scss';
 
 const pages = [
@@ -11,14 +12,29 @@ const pages = [
 ];
 
 const App: React.FC = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  // Function to handle login (You can replace this with your actual login logic)
+  const handleLogin = (username: string, password: string) => {
+    if (username === 'admin' && password === 'admin') {
+      setIsLoggedIn(true);
+    }
+  };
+
   return (
     <BrowserRouter>
-      <Navigation pages={pages} />
-      <Switch>
-        {pages.map((page) => (
-          <Route exact path={page.link} key={page.label} component={page.component} />
-        ))}
-      </Switch>
+      {window.location.href.includes('preprod') && !isLoggedIn ? (
+        <Login onLogin={handleLogin} />
+      ) : (
+        <>
+          <Navigation pages={pages} />
+          <Switch>
+            {pages.map((page) => (
+              <Route exact path={page.link} key={page.label} component={page.component} />
+            ))}
+          </Switch>
+        </>
+      )}
     </BrowserRouter>
   );
 };
---
+++ app/src/components/login.tsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+
+interface LoginProps {
+  onLogin: (username: string, password: string) => void;
+}
+
+const Login: React.FC<LoginProps> = ({ onLogin }) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    // Call the onLogin function passed from the App component
+    onLogin(username, password);
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <input
+        type="text"
+        placeholder="Username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  );
+};
+
+export default Login;
